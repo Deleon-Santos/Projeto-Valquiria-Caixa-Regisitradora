e4cdd20991812928c7390aabd15942cd5ed84553
Deleon <delps.santos1987@gmail.com>
2024-02-11 08:23:25 -0300

    inserção de tabelas

--- novo app vendas.py
+++ novo app vendas.py
@@ -25,12 +25,10 @@ except FileNotFoundError:
    
 
 # =============================================================================================================
-
 def remover(valor_a_pagar ):
     try:  # leia a lista1 e encontre o item informado
         remove_item = int(sg.popup_get_text('Remover o Item', font=("Any", 18)))
         print(f"item removido{remove_item}")
-
         for indice, material in enumerate(carrinho):
             if indice == remove_item:
                 valor_a_pagar -= material[6]
@@ -38,75 +36,58 @@ def remover(valor_a_pagar ):
                 material[5]*=-1
                 material[6]*=-1
                 window["-TABELA-"].update(values=carrinho)
-                
                 return valor_a_pagar
-        #sg.popup("Item não encontrado", font=("Any", 18))
-                
-        return valor_a_pagar
     except:
         sg.popup("Rejeitar Item", font=("Any", 18))
         return valor_a_pagar
 
-
-
-
 # =====================================================================================================================
-def pagar(soma2):
+def pagar(valor_a_pagar):
     frame6=[[sg.Text("Valor Total da Compra ", size=(28, 1), font=("Any", 12)),
-        sg.Text(f"R$ {soma2:.2f}", size=(18, 1), justification='right', key="valor", font=("Any", 18))],
+        sg.Text(f"R$ {valor_a_pagar:.2f}", size=(18, 1), justification='right', key="valor", font=("Any", 18))],
         [sg.Text("Valor Recolhido ", size=(28, 1), font=("Any", 12)),
         sg.Text(f'R$ 0.00', size=(18, 1), key="recebido", justification='right', font=("Any", 18))],
         [sg.Text("Troco Devolvido ", size=(28, 1), font=("Any", 12)),
         sg.Text(f'R$ 0.00', size=(18, 1), key="R$", justification='right', font=("Any", 18))],
         [sg.Text("", size=(10, 1))],
         [sg.Button('CARTAO', size=(20, 1)), sg.Button('PIX', size=(20, 1)), sg.Button('DINHEIRO', size=(20, 1))],
-
-    ]
+]
     layout = [
         [sg.Text("CONDIÇÃO DE PAGAMENTO", size=(35, 1), justification='center', font=("Any", 18))],
         [sg.Text("",size=(8,1)),sg.Image(filename="111.png",size=(400,100))],
         [sg.Text("", size=(10, 1))],
         [sg.Frame("",frame6)]
-
-    ]
-
+]
     window = sg.Window("PAGAMENTO", layout, finalize=True)
-
     while True:
-        
         event, values = window.read()
-        pago = soma2
+        pago = valor_a_pagar
         troco = 0
-        window["valor"].update(f"R$ {soma2:.2f}")
-
+        window["valor"].update(f"R$ {valor_a_pagar:.2f}")
         if event == (sg.WIN_CLOSED):
             sg.popup("Cancelar forma de Pagamento", font=("Any", 12))
             #return soma2
-
-
         elif event in ("CARTAO", "PIX"):  # para cartão e pix o valor e descontado itegralmente
-            if soma2 > 0:  # somente se o subtotal existir e for maior que "0"
-                soma2 = 0
-                window["valor"].update(f"R$ {soma2:.2f}")
+            if valor_a_pagar > 0:  # somente se o subtotal existir e for maior que "0"
+                valor_a_pagar = 0
+                window["valor"].update(f"R$ {valor_a_pagar:.2f}")
                 window["recebido"].update(f"R$ {pago:.2f}")
                 sg.popup("Pagamento Autorizado", font=("Any", 12))
-                return soma2
+                return valor_a_pagar
                 #break
-
         elif event == "DINHEIRO":
             try:
                 dinheiro = sg.popup_get_text("Valor Recebido", font=("Any", 12))
                 if dinheiro is not None:  # verifica se tem valores
                     dinheiro = float(dinheiro)
-                    if dinheiro >= soma2:
-                        troco = dinheiro - soma2
-                        soma2 = 0
-                        window["valor"].update(f"R$ {soma2:.2f}")
+                    if dinheiro >= valor_a_pagar:
+                        troco = dinheiro - valor_a_pagar
+                        valor_a_pagar = 0
+                        window["valor"].update(f"R$ {valor_a_pagar:.2f}")
                         window["recebido"].update(f"R$ {dinheiro:.2f}")
                         window["R$"].update(f"R$ {troco:.2f}")
-
                         sg.popup("Pagamento efetuado com sucesso", font=("Any", 18))
-                        return soma2  # desconta o subtotal e retorna o troco
+                        return valor_a_pagar  # desconta o subtotal e retorna o troco
                         #break
                     else:
                         sg.popup("Valor Insuficiente", font=("Any", 18))
@@ -119,7 +100,6 @@ def pagar(soma2):
         break
     window.close()
 
-
 # ===========================================================================================================
 def achar(material):
     for lanche in dic:
@@ -128,7 +108,7 @@ def achar(material):
     sg.popup("Erro em Produto", title="Erro", font=("Any", 18))
     return False
 
-
+# ===========================================================================================================
 def limpar_saida():
     carrinho.clear()
     window["com"].update("")
@@ -136,25 +116,49 @@ def limpar_saida():
     window["unitario"].update("")
     window["subtotal"].update("")
     window["descricao"].update("")
-    
-
 # limpa os campos sempre que uma nova função e chamada
-def venda_cupom(cupom):
-                                                            
+    
+# ===========================================================================================================
+def venda_cupom():
+    titulos = ["Item","Cod","    EAN    "," Descrição do Produto","QTD","PUni R$","Preço R$"]
+    layout=[[sg.T("VENDA CUPOM",size=(25,1),font=("any",20))],
+            [sg.T("N° Cupom",size=(20,1)),sg.T("Valor da Compra")],
+            [sg.I("1",key="-CUPOM-",size=(10,1)),sg.I(key="-VALOR-")],
+            [sg.Table(values=pesquisa_cupom, headings=titulos, max_col_width=10, auto_size_columns=True,
+            display_row_numbers=False, justification="right",text_color="black",font=("Any",11),background_color="lightyellow", num_rows=25, key="-TABELA-", row_height=20)],
+            [sg.B("PESQUISAR",size=(10,1)),sg.B("SAIR",size=(10,1))],
+    ]  
+    window = sg.Window("VENDA CUPOM",layout,finalize=True)                                                      
     while True:
         try:
+            event,values = window.read()
+            if event in (sg.WIN_CLOSED,"SAIR"):
+                break
+            cupom=values["-CUPOM-"]
+            if not cupom:
+                sg.popup("Digite o valor da pesquisa")
+                    
+                continue
+            if event=="PESQUISAR":
+                pesquisa_cupom.clear()
             
                 
-            for indice in enumerate(lista_cupom):
-                if indice == cupom:
-                    pesquisa_cupom.append(indice)
-                    print(pesquisa_cupom)
-                                       
-                    return True
+                print(f"o numero do cupos{cupom}")
+                
+                for indice,compra in enumerate(lista_cupom):
+                    if indice  == int(cupom):
+                        pesquisa_cupom.append(compra)
+                        print(pesquisa_cupom)
+                        window["-TABELA-"].print(values=pesquisa_cupom) 
+                        #window["-TABELA-"].update(values=compra)   
+                        l=len(pesquisa_cupom)
+                        print(l)
+                        indice +1            
+                        break
         except:
             sg.Text("Não Encontrado!", font=("Any", 18))           
 
-
+    window.close()
 
 
 # ===================================== Inicio do programa principal======================================================================
@@ -178,13 +182,13 @@ bloco_2=[   [sg.Text(" CAIXA FECHADO", size=(15, 1), key='caixa', font=("Any", 5
             [sg.Text('Código do Produto', size=(25, 1), font=("Any", 12)),sg.Text("", size=(35, 1)),
              sg.Text('  Quantidade', size=(10, 1), font=("Any", 12))],
             [sg.InputText(background_color='White', size=(3,2 ), key='lanche1', font=("Any", 25)),
-             sg.InputText(background_color='White', size=(14,2 ), key='ean', font=("Any", 25)),
-             sg.Text("", size=(24, 1)),sg.InputText("1", size=(8, 2), key='qtd', font=("Any", 25),justification="right")],
+             sg.InputText(background_color='White', size=(14,2 ), key='ean', font=("Any", 25), disabled=False),
+             sg.Text("", size=(24, 1)),sg.InputText("1", size=(8, 2), key='qtd', font=("Any", 25), disabled=False, justification="right")],
             [sg.Text('Descrição do Produto', size=(25, 1), font=("Any", 12))],
-            [sg.InputText(size=(38, 2), key='descricao', font=("Any", 25))],]
+            [sg.InputText(size=(38, 2), key='descricao', font=("Any", 25), disabled=False)],]
 
-bloco_3=[   [sg.Button('OK', size=(15,1),font=("Any",25)), sg.Text("", size=(10, 1)),sg.Button('DELETE', size=(15, 1),font=("Any",25))],
-            [sg.Button('PAGAR', size=(15, 1),font=("Any",25)),sg.Text("", size=(10, 1)), sg.Button('VOLTAR', size=(15, 1),font=("Any",25))],]
+bloco_3=[   [sg.Button('OK', size=(15,1),font=("Any",25), key='OK_KEY'), sg.Text("", size=(10, 1)),sg.Button('DELETE', size=(15, 1),font=("Any",25),key="DELETE_KEY")],
+            [sg.Button('PAGAR', size=(15, 1),font=("Any",25),key="PAGAR_KEY"),sg.Text("", size=(10, 1)), sg.Button('VOLTAR', size=(15, 1),font=("Any",25),key='VOLTAR_KEY')],]
 
 bloco_4=[   [sg.Image(filename="images.png",size=(695,210))],]
 bloco_5=[   [sg.Text("Operador de Caixa:",size=(17,1),font=("Any",18)),sg.Input("ADMINISTRADOR DO SISTEMA",key='LOG',size=(40, 1),font=("Any", 15))],
@@ -212,7 +216,7 @@ while True:
     if event in (sg.WIN_CLOSED, "Fechar"):
         # sg.popup("ENCERRAR",font=("Any", 18))
         break
-    elif event == "Nova Compra":
+    elif event in("F2", "Nova Compra"):
         
         n_cupom += 1
         carrinho.append(n_cupom)
@@ -224,9 +228,11 @@ while True:
         while True:
             try:
                 event, values = window.read()
-                if event =='OK': 
+                if event in('OK',"ENTER"): 
                     # verifica se ha valores nos campos de produto e quantidade
                     material = values['lanche1']
+                    if len(material)>3:
+                        sg.popup("Valor digitado esta incorreto")    
                     qtd_item = int(values["qtd"])
                     if not material:
                         sg.popup("Erro em Produto!", title="Erro", font=("Any", 18))
@@ -255,11 +261,8 @@ while True:
                             window["unitario"].update(f"{v_item_uni:.2f}")
                             window["preco"].update(f"{v_item_x_qtd:.2f}")
                             window['subtotal'].update(f" {valor_a_pagar:.2f}")
-                            
-                            
-                          
-              
-                elif event == 'DELETE':
+                                                                          
+                elif event in( 'DELETE', "DELETE_KEY"):
                     valor_a_pagar = remover(valor_a_pagar)
                     window['subtotal'].update(f"R$ {valor_a_pagar:.2f}")
                     window["-TABELA-"].update(values=carrinho)
@@ -267,8 +270,6 @@ while True:
                     cancelados = len(item_cancelado)
                      # condição para mostra o valor estornado
                     if cancelados == 1:
-                        
-                        
                         item_cancelado.clear()
                     continue
 
@@ -279,17 +280,14 @@ while True:
                         lista_cupom.extend(carrinho)
                         limpar_saida()
                         num_item = 0
-                        print(f"lista_produto{lista_cupom}")
                         break
-                    else:
-                        continue
+                    break
+
                 elif event == "VOLTAR":  # limpa todos os valores e lista local
                     carrinho.clear()
                     limpar_saida()
-
                     valor_a_pagar = 0
                     n_cupom -= 1
-                    
                     break
                 elif event == (sg.WIN_CLOSED):
                     sg.popup("ENCERRAR", font=("Any", 18))
@@ -300,7 +298,6 @@ while True:
     elif event == "Venda Total":
         limpar_saida()
         
-    
     elif event == "VOLTAR":
         window['caixa'].update('CAIXA FECHADO')
         limpar_saida()
@@ -309,13 +306,10 @@ while True:
         limpar_saida()
         
     elif event == "Venda Cupom":
-        cupom=sg.popup_get_text("Informe o item da Pesquisa")
+        #cupom=sg.popup_get_text("Informe o item da Pesquisa")
         limpar_saida()
-        cupom=venda_cupom(cupom)
-        if cupom==True:
-            window["-TABELA-"].update(values=pesquisa_cupom)
-        else:
-            sg.popup("Seu cupom não foi encontrado")
+        venda_cupom()
+        
     elif event == "Data":
         limpar_saida()
         data = sg.popup_get_text("Data", font=("Any", 18))
@@ -323,31 +317,13 @@ while True:
             window["data"].update(f'{data}')
 
     elif event == "Ajuda":
-        sg.popup_scrolled(" Sistema em desenvolvimento, algumas funcionalidades  podem  apresentar erros\n" +
-                          "e falhas inesperadas, caso ocorram problemas reportem ao desenvolvedor  atraves\n" +
-                          "do contato disponivel pra que as correções seja incorporadas ao codigo.Em brevo\n" +
-                          "apresentaremos uma bibliotca. Aguardem!\n" +
-                          "delps.santos1987@gmail.com , linkedin.com/in/deleon-santos-1b835a290\n" +
-                          "\n" +
-                          "PRIMEIROS PASSOS EM APP_VENDAS\n" +
-                          "\n" +
-                          "-Novo>\n" +
-                          "  -Pesqisar Produto: Pesquisa todos os produtos cadastrados ate o ultimo login.\n" +
-                          "  -Novo Produto:Cadastra um novo produto no banco de dados(OBS:o novo item sera\n" +
-                          "   exibido no proximo login).\n" +
-                          "  -Nova Compra: Habilita o numero de cupom e inicia a tela de registro.\n" +
-                          "       -Botão OK: Confirma a escolha do item selacionado na entrada de produto.\n" +
-                          "       -Botão DELETE: Permite excluir um item da seu carrinho de compra. \n" +
-                          "       -Botão PAGAR: Permite pagar o sub total do carrinho de compra. \n" +
-                          "       -Botão VOLTAR: Permite cancelar o cupon ou sair de qualquer menu do APP.\n" +
-                          "\n" +
-                          "-Totais>\n" +
-                          "   -Venda Cupon: Permite consultar por numero de cupon ou todos os cupons\n" +
-                          "   -Venda Total: Permite ver a venda total por item, valores  e total de  venda\n" +
-                          "\n" +
-                          "-Suporte>\n" +
-                          "   -Ajuda: Retorna um menu minimalista com as principais funcionalidades de app\n" +
-                          "   -Data: permite alterar a data no sistema", title='Ajuda')
-        continue
+        try:
+            with open('ajuda.txt', 'r') as legenda:
+                arquivo = legenda.read()
+                sg.popup_scrolled(arquivo, title="Ajuda")
+            # Seu código para ler o arquivo
+        except FileNotFoundError:
+            sg.popup("O arquivo 'comanda.txt' não foi encontrado. Verifique o caminho ou crie o arquivo.")
+               
 window.close()
 
